b1f0ebca0ad1c1f58b1f079309cb614c04b36d50
musse <1807434@brunel.ac.uk>
2020-04-05 17:55:17 +0100

    Share socials

--- Birds & Bees/app/src/main/java/com/example/android/birdsbees/views/SettingsView.java
+++ Birds & Bees/app/src/main/java/com/example/android/birdsbees/views/SettingsView.java
@@ -5,19 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.android.birdsbees.R;
 
 public class SettingsView extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_view);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "body";
+                String shareSub = "subject";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT, shareBody);
+                myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(myIntent, "Share using"));
+            }
+        });
     }
-    public void Homepage(View view) {
+
+    public void Homepage (View view) {
         Intent intent = new Intent(this, HomeView.class);
         startActivity(intent);
     }
-
 }
